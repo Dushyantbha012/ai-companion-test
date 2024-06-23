04891caba4f229c46f157172aeed921d3330ed3c
Dushyant Bhardwaj <dushyantbha012@gmail.com>
2024-06-23 15:02:24 +0530

    fix try 4

--- src/lib/memory.ts
+++ src/lib/memory.ts
@@ -32,11 +32,9 @@ export class MemoryManager {
     companionFileName: string
   ) {
     const pineconeClient = <Pinecone>this.vectorDBClient;
-    const pineconeIndex = pineconeClient.Index(
-      process.env.PINECONE_INDEX || ""
-    );
+    const pineconeIndex = pineconeClient.Index(process.env.PINECONE_INDEX!);
     const vectorStore = await PineconeStore.fromExistingIndex(
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY! }),
+      new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_AI_API_KEY! }),
       { pineconeIndex }
     );
 
